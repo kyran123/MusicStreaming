3f584a79b9c1ea7b79531f05095abb227815ed65
kyran123 <privekylefransen@hotmail.com>
2017-10-10 22:43:53 +0200

    Split up files

---
+++ PHP/register-handler.php
@@ -0,0 +1,29 @@
+<?php
+
+    function sanitizeForm($inputText, $bool) {
+        $inputText = strip_tags($inputText);
+        $inputText = str_replace(" ", "", $inputText);
+        if($bool === true){
+            $inputText = ucfirst(strtolower($inputText));
+        }
+        return $inputText;
+    }
+
+    if(isset($_POST['registerButton'])){
+        if(
+            $_POST['registerEmail'] === $_POST['registerEmailConfirm'] ||
+            $_POST['registerPassword'] === $_POST['registerPasswordConfirm']
+        ){
+            //Get and clean USERNAME variable
+            $username = sanitizeForm($_POST['registerUsername'], false);
+            //get and clean FIRSTNAME variable
+            $firstName = sanitizeForm($_POST['registerFirstName'], true);
+            //get and clean LASTNAME variable
+            $lastName = sanitizeForm($_POST['registerLastName'], true);
+            //get and clean EMAIL variable
+            $email = sanitizeForm($_POST['registerEmail'], false);
+            //get and clean PASSWORD variable
+            $password = sanitizeForm($_POST['registerPassword'], false);
+        }
+    }
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    include_once("PHP/register-handler.php");
+    include_once("PHP/register-handler.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kyrans Music Player</title>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+
+</body>
+
+<script>
+    $(function(){
+        $("body").load("Templates/Register.html");
+    });
+</script>
+</html>
