0e93992a106c942bf0eb8256d5c1ff6f9cd3dad9
kyran123 <privekylefransen@hotmail.com>
2017-10-25 13:56:43 +0200

    Added navbar items and styled them

--- Templates/Menu.php
+++ Templates/Menu.php
@@ -1,7 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Kyran
- * Date: 10/25/2017
- * Time: 12:46 PM
- */
+<div id="navBarContainer">
+    <nav class="navBar" >
+
+        <div class="group">
+
+            <div class="navItem">
+                <input name="search" class="navItemLink search" placeholder="Search" required />
+            </div>
+
+        </div>
+
+        <div class="group">
+
+            <div class="navItem">
+                <a href="browse.php" class="navItemLink">Browse</a>
+            </div>
+
+            <div class="navItem">
+                <a href="music.php" class="navItemLink">Your Music</a>
+            </div>
+
+            <div class="navItem">
+                <a href="profile.php" class="navItemLink">Kyran</a>
+            </div>
+
+        </div>
+
+    </nav>
+</div>
--- index.php
+++ index.php
@@ -39,6 +39,8 @@
         <link rel="stylesheet" type="text/css" href="Style/Register.css">
         <link rel="stylesheet" type="text/css" href="Style/Theme.css">
         <link rel="stylesheet" type="text/css" href="Style/Footer.css">
+        <link rel="stylesheet" type="text/css" href="Style/Menu.css">
+        <link rel="stylesheet" type="text/css" href="Style/Content.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -46,8 +48,10 @@
     </head>
     <body>
         <div id="mainContainer">
-            <?php include("Templates/Menu.php"); ?>
-            <?php include("Templates/Content.php"); ?>
+            <div id="topContainer">
+                <?php include("Templates/Menu.php"); ?>
+                <?php include("Templates/Content.php"); ?>
+            </div>
             <?php include("Templates/Register.php"); ?>
             <?php include("Templates/Footer.php"); ?>
         </div>
